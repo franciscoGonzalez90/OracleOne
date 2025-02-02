3fa0363fa4994c43db9f909d26acafb52356648d
francisco gonzalez <francisco.gonzalez91@hotmail.cl>
2025-02-02 01:06:06 -0300

    proyecto desafio OracleOneG8

---
+++ README.md
@@ -0,0 +1,30 @@
+# OracleOneG8 - Desafio Amigo Secreto!
+
+
+## ![Desafio](https://img.shields.io/badge/Desafio-Finalizado!-green?labelColor=grey&style=flat&logo=checkmarx)
+
+## Proyecto de desafio OracleOne del amigo secreto
+![Descripcion](https://img.shields.io/badge/Descripcion-grey?style=plastic&logo=checkmarx) 
+### Crear una app que almacene los nombres de los jugadores y que al presionar el boton de jugar aparezca el nombre de uno de los jugadores que sera el amigo secreto que le tocara!
+
+### Tecnologias utilizadas:
+* Git 
+* Javascript 
+* HTML
+* CSS
+* Trello
+
+
+![git](https://img.shields.io/badge/git-grey?style=plastic&logo=git)
+![JS](https://img.shields.io/badge/JS-grey?style=plastic&logo=javascript)
+![CSS](https://img.shields.io/badge/CSS-grey?style=plastic&logo=css3)
+![HTML](https://img.shields.io/badge/HTML-grey?style=plastic&logo=html5)
+![TRELLO](https://img.shields.io/badge/TRELLO-grey?style=plastic&logo=trello)
+
+## Tareas a Realizar:
+* Crear Repositorio Git
+* Descargar repositorio base
+* Crear en JS un arreglo para almacenar los nombres de los jugadores
+* Crear una función JS para crear amigos
+* Crear una funcion JS para actualizar el listado de amigos
+* Crear una funcion JS para sortear los nombres de los amigos
---
+++ app.js
@@ -0,0 +1,91 @@
+/****************************************************************************************/
+/*  Archivo JS que permite configurar el HTML de la app "sortear amigos"                */
+/****************************************************************************************/
+/*                             ****FUNCIONES****                                        */
+/****************************************************************************************/
+/*  agregarAmigo() -- Se agrega un nuevo amigo al arreglo amigos                        */
+/*  recorrerArreglo() -- Recorre el arreglo amigos y los lista en el HTML               */
+/*  sortearAmigo() -- Entrega el nombre de un amigo aleatoriamente                      */
+/*  verificarArrayAmigos() -- Verifica que hay amigos en el arreglo                     */
+/*  permitirSoloLetras() -- Verifica que se ingresen letras y numeros a un input*/
+/****************************************************************************************/
+
+
+//creacion arreglo de listados de amigos.-
+let amigos=[];
+
+//selectores de la app como el input de amigos, el boton de añadir y las listas para mostrar
+//los amigos agregados y el resultado del amigo secreto.-
+const inputAmigos = document.getElementById("amigo");
+const listaAmigos = document.getElementById("listaAmigos");
+const listaResultados = document.getElementById("resultado");
+
+//Se verifica que el input de amigos no se encuentre vacio de lo contrario lo almacena en el 
+//arreglo amigos.
+function agregarAmigo(){
+    if(inputAmigos.value === ""){
+        alert("Por favor, inserte un nombre.");
+    }else{
+        let validarInputAmigo = permitirSoloLetras(inputAmigos.value);
+        if(validarInputAmigo){
+            amigos.push(inputAmigos.value);
+            recorrerArreglo();
+        }else{
+            alert("El nombre no cumple con las politicas, deben ser solo letras, minimo 3 y maximo 15!");
+        }
+      
+        inputAmigos.value="";
+    }
+}
+
+//Recorre el arreglo y agrega los amigos que hay disponible al HTML.
+function recorrerArreglo(){
+    listaAmigos.innerHTML="";
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.innerHTML=amigos[i];
+        listaAmigos.appendChild(li);
+    }    
+}
+
+function sortearAmigo(){
+    //limpar las listas antes de mostrar los datos
+    listaAmigos.innerHTML="";
+    listaResultados.innerHTML="";
+
+    //se agregan los atributos disabled al input y boton de agregar amigos
+    //al presionar el boton de sortear amigos
+    inputAmigos.setAttribute("disabled", "disabled");
+    inputAmigos.nextElementSibling.setAttribute("disabled", "disabled");
+    //verifica si el arreglo tiene amigos en el arreglo
+    //SI tiene amigos en el arreglo, sortea un indice al azar del arreglo y lo muestra en el HTML
+    //y elimina ese amigo del arreglo para que cuando vuelva a sortear solo se muestren los amigos
+    //que quedan.
+    //en el caso de NO tener amigos envia una alerta que no existen amigos en el arreglo y
+    //se activa el input y boton para nuevamente ingresar amigos al arreglo y volver a jugar
+    if(verificarArrayAmigos()){
+        let indiceAmigoSorteado = Math.floor((Math.random()*amigos.length));
+        listaResultados.append(amigos[indiceAmigoSorteado]);
+        amigos.splice(indiceAmigoSorteado,1);
+        console.log(amigos);
+    }else{
+        alert("No hay amigos para sortear!");
+        inputAmigos.removeAttribute("disabled");
+        inputAmigos.nextElementSibling.removeAttribute("disabled");
+    }
+    
+}
+
+//ver si el arreglo amigos se encuentra con datos
+function verificarArrayAmigos(){
+    return Array.isArray(amigos) && amigos.length !== 0;
+}
+
+//funcion que permite verificar que el input que le entregemos solo contenga letras y numeros
+function permitirSoloLetras(input){
+    const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ]\d{3,15}$/;
+    return regex.test(input);
+}
+
+
+
